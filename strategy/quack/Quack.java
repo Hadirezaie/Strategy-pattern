ccbf5fd6615bc8670abfb810873288e911a87d21
Hadi <hadi.rezaie2015@gmail.com>
2025-10-25 10:36:47 +0430

    Quack class created and overrided quack method from QuackBehavior interface.

---
+++ strategy/quack/Quack.java
@@ -0,0 +1,10 @@
+package strategy.quack;
+
+import strategy.behavior.QuackBehavior;
+
+public class Quack implements QuackBehavior {
+    @Override
+    public void quack() {
+        System.out.println("Quack quack!");
+    }
+}
