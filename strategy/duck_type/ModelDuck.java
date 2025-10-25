944972eb32edd1eaf4ede97fa16369fb14880d85
Hadi <hadi.rezaie2015@gmail.com>
2025-10-25 10:40:00 +0430

    ModelDuck class created with a constructor and overrided display method from Duck class.

---
+++ strategy/duck_type/ModelDuck.java
@@ -0,0 +1,17 @@
+package strategy.duck_type;
+
+import strategy.Duck;
+import strategy.fly.FlyNoWay;
+import strategy.quack.Quack;
+
+public class ModelDuck extends Duck {
+    public ModelDuck() {
+        flyBehavior = new FlyNoWay();
+        quackBehavior = new Quack();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a model duck!");
+    }
+}
