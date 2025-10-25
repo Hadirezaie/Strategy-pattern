aabe4a4439cd1e2b077fbc341e77df5fa484592b
Hadi <hadi.rezaie2015@gmail.com>
2025-10-25 10:33:38 +0430

    Duck class created with performFly, performQuck, swim, display methods and sertters for dymanic behavior changes at run time.

---
+++ strategy/Duck.java
@@ -0,0 +1,33 @@
+package strategy;
+
+import strategy.behavior.FlyBehavior;
+import strategy.behavior.QuackBehavior;
+
+// Main class -> Duck
+public abstract class Duck {
+    protected FlyBehavior flyBehavior;
+    protected QuackBehavior quackBehavior;
+
+    public void performFly() {
+        flyBehavior.fly();
+    }
+
+    public void performQuack() {
+        quackBehavior.quack();
+    }
+
+    public void swim() {
+        System.out.println("All ducks float, even decoys!");
+    }
+
+    public abstract void display();
+
+    // Setters for dynamic behavior changes
+    public void setFlyBehavior(FlyBehavior fb) {
+        flyBehavior = fb;
+    }
+
+    public void setQuackBehavior(QuackBehavior qb) {
+        quackBehavior = qb;
+    }
+}
