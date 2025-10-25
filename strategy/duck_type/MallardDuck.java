992077f042fc2162035a2ced48fe17a4e197b8bb
Hadi <hadi.rezaie2015@gmail.com>
2025-10-25 10:39:46 +0430

    MallardDuck class created with a constructor and overrided display method from Duck class.

---
+++ strategy/duck_type/MallardDuck.java
@@ -0,0 +1,17 @@
+package strategy.duck_type;
+
+import strategy.Duck;
+import strategy.fly.FlyWithWings;
+import strategy.quack.Quack;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("I'm a real Mallard duck");
+    }
+}
